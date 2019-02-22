543d86c7203754114487264affedd783a4cb632d
unknown <user@userPC.nm.local>
2019-02-22 18:54:45 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/myimage.png') {
+      myImage.setAttribute ('src','img/myimage2.png');
+    } else {
+      myImage.setAttribute ('src','img/myimage.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+var name = localStorage.getItem('name');
+function hello(name)
+{
+  if(name === null)
+  {
+    myHeading.innerHTML = 'Привет, незнакомец';
+  }
+  else
+  {
+    myHeading.innerHTML = 'Привет, ' + name;
+  }
+}
+
+function setUserName()
+{
+  var myName = prompt('Please enter your name');
+  if(myName === null)
+  {
+    if(name === null)
+    {
+      myName = 'незнакомец';
+    }
+    else
+    {
+      myName = name;
+    }
+  }
+  else
+  {
+    localStorage.setItem('name', myName);
+  }
+  hello(myName);
+}
+
+if(!name)
+{
+  setUserName();
+}
+else
+{
+  hello(name); 
+}
+
+myButton.onclick = function()
+{
+  setUserName();
+}
